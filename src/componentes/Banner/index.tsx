fee4e7825755fa3a22a65363cf1dcd2f356a8109
Gui <guilhermedoell@outlook.com>
2023-04-14 15:57:22 -0300

    first commit

---
+++ src/componentes/Banner/index.tsx
@@ -0,0 +1,18 @@
+import './Banner.css'
+
+interface BannerProps {
+  enderecoImagem: string
+  textoAlternativo?: string
+}
+
+export const Banner = ({ enderecoImagem, textoAlternativo } :BannerProps) => {
+  // JSX
+  return (
+    <header className="banner">
+      {/*<img src="" alt="O banner principal da página do organo"/>*/}
+      <img src={enderecoImagem} alt={textoAlternativo}/>
+    </header>
+  )
+}
+
+export default Banner
